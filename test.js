6211427656bc644bda11854b5eeb507b330ceacd
portspasy <t.culi91@gmail.com>
2018-04-17 09:31:29 +0200

    3rd commit

--- test.js
+++ test.js
@@ -34,8 +34,50 @@ var age = 26;
 var isMarried = false;
 
 
-if (isMarried === true) {
+if (isMarried = true) {
     console.log(name + " is married!");
 } else {
     console.log(name + " will hopefully marry soon ;) ");
 }
+
+
+if ( 22 == "22") {
+    console.log("this is true");
+} else {
+    console.log("this is false");
+}
+
+
+
+if (22 === "22") {
+    console.log("this is true");
+} else {
+    console.log("this is false"); // False couse one is number one is a string!
+}
+
+
+
+// TEST 
+
+var heightJohn = 178;
+var ageJohn = 24;
+var scoreJohn;
+
+var heightMark = 172;
+var ageMark = 25;
+var scoreMark;
+
+scoreJohn = heightJohn + 5 * ageJohn;
+console.log(scoreJohn);
+
+scoreMark = heightMark + 5 * ageMark;
+console.log(scoreMark);
+
+if (scoreJohn < scoreMark){
+    console.log("Mark wins this silly game with " + scoreMark + " points!");
+} else if (scoreJohn === scoreMark) {
+    console.log("WOW!!! It's a draw!");
+} else {
+    console.log("John wins this silly game with " + scoreJohn + " points!");
+}
+
